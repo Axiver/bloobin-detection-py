ef66eddc3f180a8fda16a09d4fed19bb7423c3c3
Xavier Teo <xavierteo26@gmail.com>
2024-11-12 02:50:23 +0800

    feat: pi camera test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # Import dependencies
 from gpiozero import DistanceSensor
-from signal import pause
+from picamera import PiCamera
 from time import sleep
 
 # 
@@ -12,15 +12,33 @@ from time import sleep
 THRESHOLD_DISTANCE = 40; # in cm
 
 # Functions
-## Main
-def main():
+## Initialise sensors
+def initSensors():
+  global sensor, camera
+
+  # Initialise sensors
+  print("Initialising sensors")
+
   # Create a new ultrasonic sensor
   sensor = DistanceSensor(trigger=23, echo=24, threshold_distance=THRESHOLD_DISTANCE / 100)
+
+  # Initialise the camera
+  camera = PiCamera()
+  
+  # Sleep for 2 seconds to allow the camera to warm up
+  sleep(2)
+  print("Sensors initialised")
+
+## Main
+def main():
+  # Initialise sensors
+  initSensors()
 
   while True:
     # Check if the distance is within the threshold distance
     if (sensor.distance < THRESHOLD_DISTANCE / 100):
       print("Within threshold distance")
+      camera.capture('./images/test.jpg')
 
     print(f"Distance: {sensor.distance * 100} cm")
     # Sleep for 10ms
