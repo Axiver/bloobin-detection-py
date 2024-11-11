65f3a67be3b2a998f36456b463bbf2f83c7c87bb
Xavier Teo <xavierteo26@gmail.com>
2024-11-12 02:29:18 +0800

    refactor: use while event loop instead of callbacks

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # Import dependencies
 from gpiozero import DistanceSensor
+from signal import pause
 from time import sleep
 
 # 
@@ -14,8 +15,15 @@ THRESHOLD_DISTANCE = 40; # in cm
 ## Main
 def main():
   # Create a new ultrasonic sensor
-  sensor = DistanceSensor(trigger=23, echo=24, threshold_distance=THRESHOLD_DISTANCE)
-  sensor.when_activated = lambda: print(f"Activated; Distance: {sensor.distance * 100} cm")
-  sensor.when_deactivated = lambda: print(f"Deactivated; Distance: {sensor.distance * 100} cm")
+  sensor = DistanceSensor(trigger=23, echo=24, threshold_distance=THRESHOLD_DISTANCE / 100)
+
+  while True:
+    # Check if the distance is within the threshold distance
+    if (sensor.distance < THRESHOLD_DISTANCE / 100):
+      print("Within threshold distance")
+
+    print(f"Distance: {sensor.distance * 100} cm")
+    # Sleep for 10ms
+    sleep(0.01)
 
 main()
