431cbdbedf5b5999ce2fa03beebae65e7339ed0a
Xavier Teo <xavierteo26@gmail.com>
2024-11-11 22:09:12 +0800

    feat: distance sensor test

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# Import dependencies
+from gpiozero import DistanceSensor
+from time import sleep
+
+# 
+# GPIO Mappings
+# Ultrasonic Sensor - Trigger: 23, Echo: 24
+# 
+
+# Constants
+THRESHOLD_DISTANCE = 40; # in cm
+
+# Functions
+## Main
+def main():
+  # Create a new ultrasonic sensor
+  sensor = DistanceSensor(trigger=23, echo=24, threshold_distance=THRESHOLD_DISTANCE)
+  sensor.when_activated = lambda: print(f"Activated; Distance: {sensor.distance * 100} cm")
+  sensor.when_deactivated = lambda: print(f"Deactivated; Distance: {sensor.distance * 100} cm")
